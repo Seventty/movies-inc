c9f8261d1e875f6a0fde09ef81bf52de6fb6a8c1
Nizar Sanchez <nizar4790k@gmail.com>
2021-05-28 10:25:33 -0400

    request token is fetched from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React,{useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [requestToken,setRequestToken]= useState('');
+  const API_KEY = '0c0eca362b40cdc4df74e5a1e2c95781';
+
+  
+    async function fetchToken(){
+    
+    const promise = await fetch("https://api.themoviedb.org/3/authentication/token/new?api_key="+API_KEY)
+    const result = await promise.json();
+    const token = result.request_token;
+    setRequestToken(token);
+    
+  }
+
+  
+  useEffect(async ()=>{
+    await fetchToken();
+    console.log(requestToken)
+  
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
