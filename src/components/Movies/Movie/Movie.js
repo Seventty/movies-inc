45be373cc564f25e6d0abf794409a672eccf59fc
Nizar Sanchez <nizar4790k@gmail.com>
2021-05-29 23:58:18 -0400

    Details page is working correctly

--- src/components/Movies/Movie/Movie.js
+++ src/components/Movies/Movie/Movie.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 const Movie = ({movie}) => {
 
+    const history = useHistory();
+
+    const goToDetails = () => {
+        history.push(`Movies/${movie.id}`);
+      };
+    
+    
 
     return (
         
-        <div className="card h-50" style={{"width":"18 rem"}}>
+        <div className="card h-100" style={{"width":"18 rem"}}>
             <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2"+movie.poster_path} className="card-img-top" alt="..."></img>
             <div className="card-body">
                 <h5 className="card-title">{movie.title}</h5>
@@ -14,12 +22,10 @@ const Movie = ({movie}) => {
             <ul className="list-group list-group-flush">
                 <li className="list-group-item"><b>Release Date: </b>{movie.release_date}</li>
                 <li className="list-group-item"><b>Average Vote: </b>{movie.vote_average}</li>
+                <li className="list-group-item"><button className="btn btn-info" style={{"color":"white"}} onClick= {()=>{goToDetails()}} >View details</button></li>
                 
             </ul>
-            <div className="card-body">
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
+            
         </div>
         
     );
---
+++ src/components/Movies/MoviesDetails.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import config from '../../config';
+import MovieList from './MovieList/MovieList';
+import "./MoviesDetails.css"
+import {useHistory} from 'react-router-dom';
+
+const MovieDetails = ({ match }) => {
+
+
+
+    const [movie, setMovie] = useState('');
+    const [idx, setIdx] = useState(Number(match.params.id));
+    const [genres, setGenres] = useState([]);
+
+    const history = useHistory();
+
+    const getBacktoMovies = ()=>{
+        history.push('/Home')
+    }
+
+    useEffect(() => {
+
+        const fetchMovie = async () => {
+            const api_key = config[process.env.NODE_ENV].api_key;
+            const response = await fetch("https://api.themoviedb.org/3/movie/" + idx.toString() + "?api_key=" + api_key + "&language=en-US")
+            const result = await response.json();
+
+
+            setMovie(result);
+            setGenres(result.genres)
+
+
+
+        }
+        fetchMovie();
+
+    }, [])
+
+
+    return (
+        <div className="container">
+            <div className="row align-items-center">
+
+                <div className="col">
+                    <div className="card mb-3" style={{ "width": "18 rem" }}>
+                        <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + movie.poster_path} className="card-img-top" alt="..."></img>
+                        <div className="card-body">
+                            <h5 className="card-title">{movie.title}</h5>
+                            <p className="card-text">{movie.overview}.</p>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><b>Release Date: </b>{movie.release_date}</li>
+                            <li className="list-group-item"><b>Average Vote: </b>{movie.vote_average}</li>
+
+                        </ul>
+
+                        <div className="card-body">
+                            <b>Genres:</b>
+                            <ul>
+                                {genres.map((genre) => {
+                                    return (<li>{genre.name}</li>)
+                                })}
+
+
+                            </ul>
+
+                            
+
+                        </div>
+
+                        <ul className="list-group list-group-flush">
+                                <li className="list-group-item"><button className="btn btn-danger" style={{ "color": "white" }} onClick={()=>{getBacktoMovies()}}>Get back</button></li>
+                            </ul>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+
+};
+
+export default MovieDetails;
