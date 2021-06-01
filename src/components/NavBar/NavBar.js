7df9edf8d148518c0a18fc2ddb5765ad6e6c0853
Nizar Sanchez <nizar4790k@gmail.com>
2021-06-01 10:52:04 -0400

    nav bar added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,24 @@
+//IMPORTANT: THIS COMPONENT WORKS WELL WITH BOOTSTAP 4
+
+
+import { useHistory } from "react-router-dom";
+const NavBar = ({username}) => {
+
+  const history = useHistory();
+    
+    return (
+        <header className="header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a className="navbar-brand" href="/ChatBox">Movies.Inc</a>
+        <p className="ml-auto mr-1">{username}</p>
+        <button type="button" id="signout" className="btn btn-danger ml-auto mr-1" onClick={()=>{history.push("/")}}>Sign out</button>
+       
+      </nav>
+        </header>
+       
+      
+    );
+}
+
+
+export default NavBar;
